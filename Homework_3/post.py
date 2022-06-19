5d28b90b164753438595c887c38aa1d9ab2b4f51
Johannes Grafen <johannes.grafen@rwth-aachen.de>
2022-06-19 15:33:10 +0200

    scaling plots for fine mesh + timings for scalar opt and other stuff

--- Homework_3/post.py
+++ Homework_3/post.py
@@ -4,9 +4,11 @@ import os
 import re
 import sys
 import matplotlib.pyplot as plt
+import statistics as st
 
 # Extract timings
 timings = {}
+averageTimes = {}
 
 os.chdir(os.getcwd() + "/" + sys.argv[1])
 directory = os.fsencode(os.getcwd())
@@ -15,6 +17,8 @@ for file in os.listdir(directory):
      match = re.match("out_(\d+)CPU\.\d+\.txt",filename)
      if match: 
         cpu = int(match.group(1))
+        if cpu not in timings.keys():
+            timings[cpu] = []
         #print("search in file: out_" + str(cpu) +"CPU...")
         with open(filename,'r') as f:
             data = f.readlines()
@@ -22,7 +26,7 @@ for file in os.listdir(directory):
                 match2 = re.match("Elapsed time is (\d+\.\d+)", line)
                 if match2:
                     #print(match2.group(1))
-                    timings[cpu] = match2.group(1) 
+                    timings[cpu].append(match2.group(1)) 
      else:
          continue
 
@@ -33,8 +37,11 @@ x = []
 y = []
 for i in sorted(timings.items()):
     x.append(int(i[0]))
-    y.append(float(i[1]))
+    y.append(st.mean([float(k) for k in i[1]]))
+    
 print(x)
 print(y)
+plt.xlabel("number of processors ")
+plt.ylabel("time [s]")
 plt.plot(x,y)
 plt.savefig(os.getcwd() + "/Scaling.eps")
